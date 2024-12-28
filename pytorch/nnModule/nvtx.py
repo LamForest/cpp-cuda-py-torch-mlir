a4b647cdb54dc4fc5a0a3ac2dc382ed8aecdb3f1
TL Gao <gaotianlinstar@gmail.com>
2024-12-29 01:53:45 +0800

    tag仿照torch.cuda.nvtx的使用方式

--- pytorch/nnModule/nvtx.py
+++ pytorch/nnModule/nvtx.py
@@ -2,8 +2,11 @@ import torch
 import sys
 sys.path.append("../torchviz")
 from viz import viz_graph
+import torch
 
-class XrayBackwardHook(torch.autograd.Function):
+STACK = []
+
+class TagBackwardHook(torch.autograd.Function):
     @staticmethod
     def forward(ctx, *args):
         ctx.mark_non_differentiable(*[arg for arg in args if not arg.requires_grad])
@@ -27,11 +30,11 @@ def apply_hook(args, fn):
     if not (requires_grad and torch.is_grad_enabled()):
         return args
     
-    new_tensors = XrayBackwardHook.apply(*tensors)
+    new_tensors = TagBackwardHook.apply(*tensors)
 
     
     assert len(new_tensors) > 0
-    grad_fns = [t.grad_fn for t in new_tensors if t.grad_fn is not None and t.grad_fn.name() == "XrayBackwardHookBackward"]
+    grad_fns = [t.grad_fn for t in new_tensors if t.grad_fn is not None and t.grad_fn.name() == "TagBackwardHookBackward"]
     
     print(f"{grad_fns=}")
     
@@ -41,43 +44,46 @@ def apply_hook(args, fn):
     for idx, val in zip(tensors_idx, new_tensors):
         arg_list[idx] = val
     return tuple(arg_list)
+
     
-def backward_after_hook(*args, **kwargs):
-    print(f"backward_after_hook")
-def backward_pre_hook(*args, **kwargs):
-    print(f"backward_pre_hook")
-    
-def begin_tag(*args, tag:str):
-    args = apply_hook(args, fn=lambda *_a, **_b: print(f'[BWD END {tag}]'))
+def tag_push(*args, tag:str):
+    STACK.append(tag)
+    args = apply_hook(args, fn=lambda *_a, **_b: print(f'[END BACKWARD]: {tag}')) #[BEGIN FORWARD]
+    print(f"[BEGIN_FORWARD]: {tag}")
     if len(args) == 1: #简单粗暴，先这么用吧
         return args[0]
     return args
 
-def end_tag(ret, tag:str):
+def tag_pop(ret, tag=None):
+    #如果用户传入tag，那么验证是否期望的tag与栈顶的tag一致
+    if tag is not None:
+        assert tag == STACK[-1]
+    tag = STACK.pop()
+    
     is_tuple = True
     if not isinstance(ret, tuple):
         ret = (ret,)
         is_tuple = False
-    ret = apply_hook(ret, fn=lambda *_a, **_b: print(f'[BWD BEGIN {tag}]'))
+    ret = apply_hook(ret, fn=lambda *_a, **_b: print(f'[BEGIN BACKWARD]: {tag}'))
+    print(f"[END FORWARD]: {tag}")
+    
     if not is_tuple:
         ret = ret[0]
     return ret
 
 
-def xray_tag(tag="unknown tag"):
+def tag(tag="unknown tag"):
     def decorator(func):
         def wrapper(*args, **kwargs):
             if kwargs is None:
                 kwargs = {}
             assert len(kwargs) == 0, f"暂不支持kwargs hook"
-            args = begin_tag(*args, tag=tag)
+            args = tag_push(*args, tag=tag)
             if not isinstance(args, tuple):
                 args = (args,)
-            print(f"[FWD BEGIN {tag}]")
             ret = func(*args, **kwargs)
-            print(f"[FWD END {tag}]")
             
-            ret = end_tag(ret, tag=tag)
+            ret = tag_pop(ret, tag=tag)
             return ret
         return wrapper
     return decorator
@@ -91,19 +97,19 @@ x1, x2 = (
 )
 x = torch.randn(2, 3, requires_grad=True)
 
-@xray_tag("fff")
+@tag("fff")
 def f(x, y):
     out = (x -y).mul(5)
     return out
 
 
 def f1(x, y):
-    x, y = begin_tag(x, y, tag="fff")
+    x, y = tag_push(x, y, tag="fff")
     out = (x -y).mul(5)
-    out = end_tag(out, tag="fff")
+    out = tag_pop(out)
     return out
 
-@xray_tag("ff2")
+@tag("ff2")
 def f2(x):
     return x.sub(5).div(2)
 
